f6e714d30665c656a0b212231b4046033a645752
fauzandotme <fauzan@mantolitoli.sch.id>
2017-01-25 11:20:27 +0700

    add method request

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ function curl(link, options) {
   let include = '-i '; // (options.include) ? '-i ' : '';
   let cookie = (options.cookie) ? `-H 'Cookie: ${options.cookie}' ` : '';
   let location = (options.redirect) ? '-L ' : '';
-  let command = `curl -g ${useragent+head_only+include+headers+cookie+location+post+referer}'${url}'`;
+  let method = (options.method) ? `-X ${options.method} ` : '';
+  let command = `curl -g ${useragent+head_only+include+headers+cookie+location+post+referer+method}'${url}'`;
   return new Promise((resolve, reject) => {
     exec(command,{maxBuffer: 1024 * 5000}, (err, res) => {
       if(err) reject(err);
